0bf22512e26fb4cd773e42217fcb613b89578b02
Eric Hexter <eric.hexter@gmail.com>
2013-01-11 00:27:13 -0600

    Initial Commit

---
+++ WebConfigTransformRunner/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebConfigTransformRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Wrong number of arguments");
+                Console.WriteLine("WebConfigTransformer ConfigFilename TransformFilename ResultFilename");
+                Environment.Exit(1);
+            }
+            if (!System.IO.File.Exists(args[0]) && !System.IO.File.Exists(args[1]))
+            {
+                Console.WriteLine("The config or transform file do not exist!");
+                Environment.Exit(2);
+            }
+            using (var doc = new Microsoft.Web.XmlTransform.XmlTransformableDocument())
+                {
+                    doc.Load(args[0]);
+                    using (var tranform = new Microsoft.Web.XmlTransform.XmlTransformation(args[1]))
+                    {
+                        if (tranform.Apply(doc))
+                        {
+                            doc.Save(args[2]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Could not apply transform");
+                            Environment.Exit(3);
+                        }
+                    }
+                }
+            }
+        
+    }
+}
---
+++ WebConfigTransformRunner/ReadMe.txt
@@ -0,0 +1,15 @@
+﻿Web Config Transformation Runner -
+
+This is a commandline tool to run an ASP.Net web.config tranformation.
+
+Usage:
+
+		WebConfigTransformationRunner.exe WebConfigFilename TransformFilename OutputFilename
+
+More Information:
+		https://github.com/erichexter/WebConfigTransformRunner
+
+Created by:
+	Eric Hexter 
+		Blog - http://lostechies.com/erichexter/
+		Twitter - http://twitter.com/ehexter		
