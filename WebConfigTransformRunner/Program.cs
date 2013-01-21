b51af8a0447d5646b07aa5917a34db15fb2d6eb4
Max Malook <community@malook.de>
2013-01-21 14:17:02 +0100

    fix boolean logic typo
    As stated in the error message _or_ is the correct operator.

--- WebConfigTransformRunner/Program.cs
+++ WebConfigTransformRunner/Program.cs
@@ -15,7 +15,7 @@ namespace WebConfigTransformRunner
                 Console.WriteLine("WebConfigTransformer ConfigFilename TransformFilename ResultFilename");
                 Environment.Exit(1);
             }
-            if (!System.IO.File.Exists(args[0]) && !System.IO.File.Exists(args[1]))
+            if (!System.IO.File.Exists(args[0]) || !System.IO.File.Exists(args[1]))
             {
                 Console.WriteLine("The config or transform file do not exist!");
                 Environment.Exit(2);
