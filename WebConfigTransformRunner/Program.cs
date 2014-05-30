5a22d0af7c09f6df6f3b31616ad976c848023866
Tjeerd-menno Douma <tdouma@wehkamp.nl>
2014-05-30 14:12:47 +0200

    1. Updated to latest non pre release version of Microsoft.Web.Xdt 2. Fixed a bug with checking file existance. (&& ==> ||)

--- WebConfigTransformRunner/Program.cs
+++ WebConfigTransformRunner/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace WebConfigTransformRunner
 {
@@ -15,7 +12,7 @@ namespace WebConfigTransformRunner
                 Console.WriteLine("WebConfigTransformer ConfigFilename TransformFilename ResultFilename");
                 Environment.Exit(1);
             }
-            if (!System.IO.File.Exists(args[0]) && !System.IO.File.Exists(args[1]))
+            if (!System.IO.File.Exists(args[0]) || !System.IO.File.Exists(args[1]))
             {
                 Console.WriteLine("The config or transform file do not exist!");
                 Environment.Exit(2);
